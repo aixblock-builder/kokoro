743290e416659a0f332fdb5fa0e97a846416ba62
tqphu27 <trn.quangph27@gmail.com>
2025-05-23 19:33:58 +0700

    .

--- load_model.py
+++ load_model.py
@@ -3,6 +3,25 @@ import shutil
 import sys
 import os
 
+def install_git_lfs():
+    try:
+        # Kiểm tra git-lfs đã được cài chưa
+        subprocess.run(['git', 'lfs', 'version'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print("git-lfs đã được cài đặt.")
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        print("Đang cài đặt git-lfs...")
+        if sys.platform.startswith('linux'):
+            subprocess.run(['apt-get', 'update'], check=True)
+            subprocess.run(['apt-get', 'install', '-y', 'git-lfs'], check=True)
+            subprocess.run(['git', 'lfs', 'install'], check=True)
+        elif sys.platform == 'darwin':
+            subprocess.run(['brew', 'install', 'git-lfs'], check=True)
+            subprocess.run(['git', 'lfs', 'install'], check=True)
+        else:
+            raise RuntimeError("Chưa hỗ trợ cài git-lfs tự động trên hệ điều hành này.")
+
+install_git_lfs()
+
 def clone_with_lfs(repo_url, repo_dir_name, target_dir):
     # Clone repo
     subprocess.run(["git", "clone", repo_url], check=True)
